f00a3c79329f89319aa0ffca0c2b46bb41ad7e84
Kitretsu <30966517+Kitretsu@users.noreply.github.com>
2017-08-15 17:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/article-one', function (req, res) {
     res.send('Article one requested and will be sreved here');
 });
 
-app.get('/article-', function (req, res) {
-    res.send('Article one requested and will be sreved here');
+app.get('/article-two', function (req, res) {
+    res.send('Article two requested and will be sreved here');
 });
 
-app.get('/article-one', function (req, res) {
-    res.send('Article one requested and will be sreved here');
+app.get('/article-three', function (req, res) {
+    res.send('Article three requested and will be sreved here');
 });
 
 app.get('/ui/style.css', function (req, res) {
